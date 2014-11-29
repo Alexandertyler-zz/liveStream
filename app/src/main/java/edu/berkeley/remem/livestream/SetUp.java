3c4549dd56d8f62f000374f4f28cf3a0b2897a44
Aleander Tyler <alexanderhtyler@gmail.com>
2014-11-29 17:58:18 -0600

    Gps

--- app/src/main/java/edu/berkeley/remem/livestream/SetUp.java
+++ app/src/main/java/edu/berkeley/remem/livestream/SetUp.java
@@ -1,6 +1,7 @@
 package edu.berkeley.remem.livestream;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +12,19 @@ import android.widget.EditText;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
 
 public class SetUp extends Activity {
 
     private static CreateBroadcast createBroadcast;
     private static JsonComm jsonComm;
+    private static LocationManager locationManager;
+    private static LocationListener locationListener;
+    private double latitude;
+    private double longitude;
 
 
     @Override
@@ -25,11 +33,38 @@ public class SetUp extends Activity {
         setContentView(R.layout.activity_set_up);
 
         createBroadcast = new CreateBroadcast();
-        //jsonComm = new JsonComm();
 
-        //Button bcastButton = (Button) findViewById(R.id.createBroadcastButton);
-        //Button jsonButton = (Button) findViewById(R.id.jsonDataButton);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
 
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+            @Override
+            public void onProviderEnabled(String s) {}
+
+            @Override
+            public void onProviderDisabled(String s) {}
+
+        };
+
+
+
+        // Register the listener with the Location Manager to receive location updates
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            // Get update every 5 seconds
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60000, 0, locationListener);
+        }
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            // Get update every 5 seconds
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 0, locationListener);
+        }
 
     }
 
@@ -56,8 +91,6 @@ public class SetUp extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-
-
     public void newBroadcast(View view) {
         System.out.println("newBroadcast clicked");
         InputStream iStream = getResources().openRawResource(getResources().getIdentifier("client_secrets", "raw", getPackageName()));
@@ -79,6 +112,35 @@ public class SetUp extends Activity {
         for (int i=0; i < jsonDataSplit.length-1; ) {
             jsonMap.put(jsonDataSplit[i++], jsonDataSplit[i++]);
         }
+
+        jsonComm = new JsonComm(url.getText().toString(), jsonMap);
+
+        try {
+            jsonComm.execute();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void sendLocation(View view) {
+        EditText url = (EditText) findViewById(R.id.urlValue);
+        //EditText jsonData = (EditText) findViewById(R.id.jsonData);
+
+        //Debug info
+        System.out.println("Received data from EditText fields:");
+        System.out.println(url.getText().toString());
+        //System.out.println(jsonData.getText().toString());
+
+
+        //String[] jsonDataSplit = jsonData.getText().toString().split(" |=");
+        Map<String, String> jsonMap = new HashMap<String, String>();
+        //for (int i=0; i < jsonDataSplit.length-1; ) {
+            //jsonMap.put(jsonDataSplit[i++], jsonDataSplit[i++]);
+        //}
+
+        jsonMap.put("longitude", String.valueOf(longitude));
+        jsonMap.put("latitude", String.valueOf(latitude));
 
         jsonComm = new JsonComm(url.getText().toString(), jsonMap);
 
